82be98ac11b8a9bfd2836be35721468617696fd1
SK Akram <skcodewizard786@gmail.com>
2025-05-03 21:59:25 +0530

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -7,7 +7,6 @@ public class TicTacToe {
     public TicTacToe() {
         board = new char[3][3];
         currentPlayer = 'X';
-        // Initialize board with spaces
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 board[i][j] = ' ';
@@ -23,15 +22,12 @@ public class TicTacToe {
     }
 
     public boolean checkWinner(char player) {
-        // Check rows
         for (int i = 0; i < 3; i++) {
             if (board[i][0] == player && board[i][1] == player && board[i][2] == player) return true;
         }
-        // Check columns
         for (int j = 0; j < 3; j++) {
             if (board[0][j] == player && board[1][j] == player && board[2][j] == player) return true;
         }
-        // Check diagonals
         if (board[0][0] == player && board[1][1] == player && board[2][2] == player) return true;
         if (board[0][2] == player && board[1][1] == player && board[2][0] == player) return true;
         return false;
